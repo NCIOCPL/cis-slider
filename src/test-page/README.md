33811027dcd32e19cb132710d470c2012f30f731
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2024-05-07 20:53:40 -0400

    (#6) Make the modal
    Closes #6

---
+++ src/test-page/README.md
@@ -0,0 +1,1 @@
+These are all the assets needed for the test page. These are not part of the actual slider.
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,30 @@
 /* global process */
+/* global __dirname */
 import { defineConfig } from 'vite';
+import autoprefixer from 'autoprefixer';
+import path from 'path';
 
 export default defineConfig({
 	base: process.env.PUBLIC_PATH ? process.env.PUBLIC_PATH : '/',
+	build: {
+		minify: true,
+		cssMinify: true,
+		rollupOptions: {
+			input: {
+				'cis-slider': 'src/main.ts',
+				'index.html': 'index.html',
+			},
+		},
+	},
+	css: {
+		postcss: {
+			plugins: [autoprefixer({})],
+		},
+		preprocessorOptions: {
+			scss: {
+				quietDeps: true,
+				includePaths: [path.join(__dirname, 'node_modules/@nciocpl/ncids-css/packages'), path.join(__dirname, 'node_modules/@nciocpl/ncids-css/uswds-packages')],
+			},
+		},
+	},
 });
