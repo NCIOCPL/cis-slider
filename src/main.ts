3cc57db9a2037da41f63db8ab94637f30ff5a630
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2024-05-07 20:53:40 -0400

    Modal is appearing. Need to fix icons, need to position buttons.

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,44 @@
-import './style.css';
-import typescriptLogo from './typescript.svg';
-import viteLogo from '/vite.svg';
-import { setupCounter } from './counter';
+/**
+ * @file This is the main import, and the file that will be called from launch.
+ *
+ * The purpose of this file is to inject a NCIDS/USWDS button into that page that
+ * will stick to the right side of the page and allow a user to click it. When the
+ * user clicks the button, a modal will appear that will give information on how to
+ * contact the Cancer Information Service (CIS).
+ */
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`;
+import content from './slider.html?raw';
+import css from './slider.scss?inline';
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
+import modal from '@uswds/uswds/js/usa-modal';
+
+// Inject slider styles.
+const sliderStyles = document.createElement('style');
+document.head.appendChild(sliderStyles);
+sliderStyles.appendChild(document.createTextNode(css));
+
+// Modal initialization.
+const initComponents = () => {
+	// Inject button
+	const button = document.createElement('a');
+	button.href = '#cis-slider-modal';
+	button.classList.add('usa-button', 'usa-button--slider-button');
+	button.textContent = 'Have Questions?';
+	button.setAttribute('data-modal-open', '');
+	button.setAttribute('aria-controls', 'cis-slider-modal');
+	document.body.appendChild(button);
+
+	// Inject modal dialog.
+	const parser = new DOMParser();
+	const dialog = parser.parseFromString(content, 'text/html');
+	document.body.appendChild(dialog.body);
+
+	const target = document.body;
+	modal.on(target);
+};
+
+if (document.readyState === 'loading') {
+	document.addEventListener('DOMContentLoaded', initComponents, { once: true });
+} else {
+	initComponents();
+}
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,24 @@
 /* global process */
+/* global __dirname */
 import { defineConfig } from 'vite';
+import autoprefixer from 'autoprefixer';
+import path from 'path';
 
 export default defineConfig({
 	base: process.env.PUBLIC_PATH ? process.env.PUBLIC_PATH : '/',
+	build: {
+		minify: true,
+		cssMinify: true,
+	},
+	css: {
+		postcss: {
+			plugins: [autoprefixer({})],
+		},
+		preprocessorOptions: {
+			scss: {
+				quietDeps: true,
+				includePaths: [path.join(__dirname, 'node_modules/@nciocpl/ncids-css/packages'), path.join(__dirname, 'node_modules/@nciocpl/ncids-css/uswds-packages')],
+			},
+		},
+	},
 });
